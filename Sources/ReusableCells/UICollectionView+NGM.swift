9c97f5b2b7b040acb2edfbb0798265d25de00d8b
Neil Macy <neil@neilmacy.co.uk>
2020-02-01 13:13:53 +0000

    documentation of register, and class and extension overviews

--- Sources/ReusableCells/UICollectionView+NGM.swift
+++ Sources/ReusableCells/UICollectionView+NGM.swift
@@ -1,17 +1,21 @@
 import UIKit
 
+/**
+ Extension for easily dequeuing `UICollectionViewCell`s.
+ */
 public extension UICollectionView {
 
-    // register and dequeue UICollectionViewCells
-
-    func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView {
+    /**
+     Convenience method to register a `UICollectionViewCell` for reuse. This is called automatically by `dequeue`.
+     */
+    internal func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView {
         register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
     }
 
     func dequeue<T: UICollectionViewCell>(for indexPath: IndexPath) -> T where T: ReusableView {
         register(T.self) // this removes the need to explicitly register a cell
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
-            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+            preconditionFailure("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
         }
 
         return cell
--- Sources/ReusableCells/UITableView+NGM.swift
+++ Sources/ReusableCells/UITableView+NGM.swift
@@ -1,32 +1,41 @@
 import UIKit
 
+/**
+ Extension for easily dequeuing `UITableViewCell`s and `UITableViewHeaderFooterView`s.
+ */
 public extension UITableView {
 
-    // register and dequeue UITableViewCells
+    // MARK: - UITableViewCell
 
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
+    /**
+     Convenience method to register a `UITableViewCell` for reuse. This is called automatically by `dequeue`.
+     */
+    internal func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
         register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
     }
 
     func dequeue<T: UITableViewCell>(for indexPath: IndexPath) -> T where T: ReusableView {
         register(T.self) // this removes the need to explicitly register a cell
         guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
-            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+            preconditionFailure("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
         }
 
         return cell
     }
 
-    // register and dequeue UITableViewHeaderFooterViews
+    // MARK: - UITableViewHeaderFooterView
 
-    func register<T: UITableViewHeaderFooterView>(_: T.Type) where T: ReusableView {
+    /**
+     Convenience method to register a `UITableViewHeaderFooterView` for reuse. This is called by default by `dequeue`.
+     */
+    internal func register<T: UITableViewHeaderFooterView>(_: T.Type) where T: ReusableView {
         register(T.self, forHeaderFooterViewReuseIdentifier: T.defaultReuseIdentifier)
     }
 
     func dequeue<T: UITableViewHeaderFooterView>(_: T.Type) -> T where T: ReusableView {
         register(T.self) // this removes the need to explicitly register a view
         guard let reusableView = dequeueReusableHeaderFooterView(withIdentifier: T.defaultReuseIdentifier) as? T else {
-            fatalError("Could not dequeue view with identifier: \(T.defaultReuseIdentifier)")
+            preconditionFailure("Could not dequeue view with identifier: \(T.defaultReuseIdentifier)")
         }
 
         return reusableView
